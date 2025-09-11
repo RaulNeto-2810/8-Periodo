01ae03d9d92ef9d92e254a4af6e945b1017eb02d
RaulNeto-2810 <raultorressneto@gmail.com>
2025-09-11 16:12:43 -0300

    correção

--- Dispositivos Móveis/Aulas/aula01/primeira-programa/app/(tabs)/index.jsx
+++ Dispositivos Móveis/Aulas/aula01/primeira-programa/app/(tabs)/index.jsx
@@ -2,11 +2,12 @@ import Button from "@/components/Button";
 import ImageViewer from "@/components/imageViewer";
 import { StyleSheet, View } from "react-native";
 import * as ImagePicker from 'expo-image-picker';
+import React, { useState } from 'react';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function HomeScreen() {
-    const [selectedImage, setSelectImage] = useState(undefined)
+    const [selectedImage, setSelectedImage] = useState(undefined)
 
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -14,24 +15,28 @@ export default function HomeScreen() {
             allowsEditing: true,
             quality: 1,
         })
-        if ( result.canceled == false) {
+        if (result.canceled === false) {
+            // Aqui você pode usar setSelectedImage para salvar a imagem selecionada
+            // Exemplo: setSelectedImage(result.assets[0].uri);
             console.log(result);
         } else {
-            alert("Você não selecionou nenhuma imagem.")
+            alert("Você não selecionou nenhuma imagem.");
         }
     }
 
     return (
-        <View style={styles.container}>
-            <View style={styles.imageContainer}>
-                <ImageViewer imgSource={PlaceholderImage} />
+        <View style={{ flex: 1 }}>
+            <View style={styles.container}>
+                <View style={styles.imageContainer}>
+                    <ImageViewer imgSource={PlaceholderImage} />
+                </View>
+            </View>
+            <View style={styles.footerContainer}>
+                <Button label={'Escolher foto'} theme={'primary'} onPress={pickImageAsync} />
+                <Button label={'Usar essa Foto'} />
             </View>
         </View>
-        <View style={styles.footerContainer}>
-            <Button label={'Escolher foto'} theme={'primary'} onPress={pickImageAsync} />
-            <Button label={'Usar essa Foto'} />
-        </View>
-    )   
+    )
 }
 
 const styles = StyleSheet.create({
@@ -45,7 +50,7 @@ const styles = StyleSheet.create({
         flex: 1
     },
     footerContainer: {
-        flex: 1/3,
+        flex: 1 / 3,
         alignItems: 'center'
     }
 })
