82bac712fe820a94bb6dd036ba548240a6c2a03a
RaulNeto-2810 <raultorressneto@gmail.com>
2025-09-11 15:14:28 -0300

    a

---
+++ Programação para Internet 2/aulas/teste/App.js
@@ -0,0 +1,86 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+
+export default function App() {
+  const [usuarios, setUsuarios] = useState([]);
+  const [carregando, setCarregando] = useState(true);
+
+  //useEffect roda enquanto o componente está sendo construído
+  useEffect(() => {
+    fetch("https://03f34d9ce726.ngrok-free.app", {
+      method: "GET",
+      headers:{
+        "User-Agent": "Chinelinho",
+        "X-Custom-Agent": "Chinelinho"
+      }
+    })
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setUsuarios(dados);
+        setCarregando(false);
+      })
+      .catch((erro) => {
+        console.log(erro);
+        setCarregando(false);
+      });
+  }, []); //[] significa que o useEffect vai rodar uma vez no início
+
+  if (carregando) {
+    return (
+      <View style={styles.container}>
+        <Text>Carregando dados...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Lista de usuários</Text>
+
+      <FlatList
+        data={usuarios}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.texto}>{item.nome}</Text>
+            <Text style={styles.email}>{item.curso}</Text>
+            <Text style={styles.email}>{item.idade}</Text>
+
+          </View>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    marginTop: 40,
+    padding: 20,
+  },
+  titulo: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  item: {
+    backgroundColor: "#f0f0f0",
+    padding: 15,
+    marginVertical: 5,
+    borderRadius: 8,
+  },
+  texto: {
+    fontSize: 18,
+  },
+  email: {
+    fontSize: 14,
+    color: "#00BFFF",
+    borderBottomWidth: 1,
+    borderColor: "#00BFFF",
+  },
+});
---
+++ Programação para Internet 2/aulas/teste/App_frutas.js
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, TextInput, Button } from 'react-native';
+
+export default function App() {
+  const frutas = [
+    { id: "1", nome: "🍌 Banana" },
+    { id: "2", nome: "🍎 Maça" },
+    { id: "3", nome: "🍊 Laranja" },
+    { id: "4", nome: "🍇 Uva" }
+  ];
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Lista de frutas</Text>
+      <FlatList
+        data={frutas}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.texto}>{item.nome}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: 40,
+    padding: 20,
+  },
+  titulo: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  item: {
+    backgroundColor: "#f0f0f0",
+    padding: 15,
+    marginVertical: 5,
+    borderRadius: 8,
+  },
+  texto: {
+    fontSize: 18,
+  },
+
+});
